61e3ce4c617b1a707923a4c1802f9b9ad4d4f8c7
DayOneAI <bambathiam88@gmail.com>
2023-06-25 14:54:48 +0200

    My_proposal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+import { useState } from 'react'
+import data from './data'
+
+// const removeList = () => {
+//   const
+// }
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
-};
-export default App;
+  const [person, setPerson] = useState(data)
+  return (
+    <section className="container">
+      {/* Rendu de la liste */}
+      <div>
+        <h3 className="title">Today annivesaires</h3>
+        <h4>Nous avons {person.length} anniversaires dans la liste</h4>
+        {person.map((pers) => {
+          return (
+            <div key={pers.id} className="person">
+              <h3>{pers.name}</h3>
+              <span>{pers.age} years</span>
+              <img src={pers.image} alt={pers.name} />
+            </div>
+          )
+        })}
+      </div>
+      {/* Clear list */}
+      <button className="btn" onClick={() => setPerson()}>
+        Clear All
+      </button>
+    </section>
+  )
+}
+export default App
